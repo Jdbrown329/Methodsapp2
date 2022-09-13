e1dec44a7a5c2deacd1fa80c857a2b8a3dde6039
Jordan Brown <j.d.brown329@gmail.com>
2022-09-13 10:58:32 -0500

    first

---
+++ Methods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+Console.WriteLine(Add(2,29));
+Console.WriteLine(Multiply(2, 29));
+Console.WriteLine(Subtract(29,2));
+static int Add(int num1, int num2)
+{
+        return num1 + num2;
+}
+static int Multiply(int num1, int num2)
+{
+    return num1 * num2;
+}
+static int Subtract (int num1, int num2)
+{
+    return num1 - num2;
+}
